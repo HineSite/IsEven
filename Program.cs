ceceb6f9635b42b7db2dd22d416ccaed064a3bad
HineSite <kevin@hine.site>
2022-03-30 23:06:14 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.WriteLine("\nDone!");
+            Console.ReadLine();
+        }
+
+        private Program()
+        {
+            IsEven(5);
+            Console.WriteLine("Even!");
+        }
+
+        public void IsEven(int num)
+        {
+            num /= (++num % 2);
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# About
+When you need a highly efficient and optimized algorithm to determine if an integer is even.
+
